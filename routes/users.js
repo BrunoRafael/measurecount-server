4044d5fc3b3c801c56d523ea85c0c69a24472899
Kalber Roberto Pereira Silva <kalber.silva@virtus.ufcg.edu.br>
2019-06-20 16:18:18 -0300

    #45665 create user edit route

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,6 @@ import UsersController from '../controllers/users';
 export default (app) => {
     const usersController = new UsersController(app.datasource.models.Users);
     app.route('/users')
-        
         .post((req, res) => {
             usersController.create(req.body)
                 .then(response => {
@@ -11,5 +10,12 @@ export default (app) => {
                     res.json(response.data);
                 })
         })
-
+    app.route('/users/:id')
+        .put((req, res) => {
+            usersController.update(req.body, req.params)
+                .then(response => {
+                    res.status(response.statusCode);
+                    res.json(response.data);
+                })
+        })
 }
